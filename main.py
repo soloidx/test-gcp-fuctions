2ab8675b11a2c3290552162857dec2041d870a45
Ider Delzo <soloidx@soloidx-macbook.local>
2020-04-15 22:27:26 -0500

    test1

--- main.py
+++ main.py
@@ -7,8 +7,15 @@ def hello_world(request):
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
+    try:
+        import googleclouddebugger
+
+        googleclouddebugger.enable()
+    except ImportError:
+        pass
+
     request_json = request.get_json()
-    a = 3/0
+    a = 3 / 0
     if request.args and 'message' in request.args:
         return request.args.get('message')
     elif request_json and 'message' in request_json:
